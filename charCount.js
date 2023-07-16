dc5dbed8f3580e53e4e131402ce1cd205e8bcfe9
Stefn Meza <stefanmeza93@gmail.com>
2023-07-16 23:14:14 +0200

    Add look back and refactor

--- charCount.js
+++ charCount.js
@@ -5,19 +5,48 @@ function charCount(str) {
   for (let i = 0; i < str.length; i++) {
     const char = str[i].toLowerCase();
 
-    // if the char is a number/letter AND is a key in object, add one to count
-    if (result[char] > 0) {
-      result[char]++;
+    // test if char is letter or number, everything else do nothing
+    if (/[a-z0-9]/.test(char)) {
+      // if the char is a key in object, add one to count
+      if (result[char] > 0) {
+        result[char]++;
 
-      // if the char is a number/letter AND is NOT in object, add it to object and set value to 1
-    } else {
-      result[char] = 1;
+        // if the char is NOT in object, add it to object and set value to 1
+      } else {
+        result[char] = 1;
+      }
     }
-
-    // if the char is something else (space, period, etc...) don't do anything
   }
   // return object at end
   return result;
 }
 
-console.log(charCount("hello"));
+function charCountImproved(str) {
+  let obj = {};
+  for (let char of str) {
+    if (isAlphaNumberic(char)) {
+      char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+}
+
+function isAlphaNumberic(char) {
+  const code = char.charCodeAt(0);
+
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A - Z)
+    !(code > 96 && code < 123) // lower alpha (a -z)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log('"hello function number 2"');
+console.log(charCountImproved("hello function number 2"));
+
+// next => section 5: problem sovling patterns
