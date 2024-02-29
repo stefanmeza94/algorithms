4bb979dc1428721950a87ba5f69e4987bf97a435
Stefan Meza <stefanmeza93@gmail.com>
2024-02-29 22:29:21 +0100

    Add optimize bubble sort; Add es5 way of swaping an element in array

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,6 @@
-// first example
+// bubble sort - first example
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
@@ -10,12 +10,47 @@ function bubbleSort(arr) {
         arr[j + 1] = temp;
       }
     }
+    console.log('ONE PASS COMPLETE');
+  }
+  return arr;
+}
+
+// console.log(bubbleSort([45, 12, 24, 4]));
+
+// bubble sort - more optimize example - where we exclude unnecessary comparison
+function bubbleSort2(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+    console.log('ONE PASS COMPLETE');
+  }
+}
+
+// bubbleSort2([2, 24, 17, 8]);
+
+// bubble sort - es5 way of swaping
+function bubbleSort3(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+    }
+    console.log('ONE PASS COMPLETE');
   }
 
   return arr;
 }
 
-console.log(bubbleSort([45, 36, 12, 24, 27, 4]));
+bubbleSort3([2, 24, 17, 8]);
 
-// more optimaze example
-// nastavi od 06:00 bubble sort implementation
+// nastavi od bubble sort optimization
